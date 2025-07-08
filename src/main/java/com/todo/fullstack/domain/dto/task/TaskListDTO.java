35e8afa7865b406f7d49a9ab8fef990221a4827f
Felix Eduardo Villanueva Condori <felixeduardobx@gmail.com>
2025-07-08 14:12:21 -0300

    Adicionado o campo Description no metodo GET

--- src/main/java/com/todo/fullstack/domain/dto/task/TaskListDTO.java
+++ src/main/java/com/todo/fullstack/domain/dto/task/TaskListDTO.java
@@ -6,9 +6,10 @@ import com.todo.fullstack.domain.entity.Task;
 public record TaskListDTO(
         Long id,
         String title,
+        String description,
         Status status,
         Long userId) {
     public TaskListDTO(Task task){
-        this(task.getId(), task.getTitle(), task.getStatus(), task.getUser().getId());
+        this(task.getId(), task.getTitle(), task.getDescription(), task.getStatus(), task.getUser().getId());
     }
 }
